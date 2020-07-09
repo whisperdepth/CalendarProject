668299d3160bcfa51e0642b256ba7f14f19d4c5d
Roman Marchenko <whisperdepth@gmail.com>
2020-07-09 23:46:57 +0300

    d

--- src/main/Week.jsx
+++ src/main/Week.jsx
@@ -3,45 +3,6 @@ import "./week.scss";
 import HoursGradation from "./hours-gradation/HoursGradation";
 import Day from "./day/Day";
 
-const days = [
-  {
-    name: "MON",
-    date: 29,
-    id: 1,
-  },
-  {
-    name: "TUE",
-    date: 30,
-    id: 2,
-  },
-  {
-    name: "WED",
-    date: 1,
-    id: 3,
-  },
-  {
-    name: "THU",
-    date: 2,
-    id: 4,
-  },
-  {
-    name: "FRI",
-    date: 3,
-    id: 5,
-  },
-  {
-    name: "SAT",
-    date: 4,
-    id: 6,
-  },
-  {
-    name: "SUN",
-    date: 5,
-    id: 7,
-  },
-];
-
-
 
 const Week = ({ week }) => {
   return (
--- src/main/day/Day.jsx
+++ src/main/day/Day.jsx
@@ -2,19 +2,39 @@ import React from "react";
 import "./day.scss";
 import Event from "./event/Event";
 
-const event = {
-  name: "Meet friends",
-  startTime: "00:45",
-  endTime: "01:45",
-  id: 1594069200000,
-};
+const events = [
+  {
+    id: 1,
+    name: "Meet friends",
+    startTime: "00:45",
+    endTime: "01:45",
+    dayTime: 1594069200000,
+  },
+  {
+    id: 2,
+    name: "Go to sleep",
+    startTime: "03:45",
+    endTime: "06:00",
+    dayTime: 1594069200000,
+  },
+
+  {
+    id: 3,
+    name: "Go to work",
+    startTime: "10:45",
+    endTime: "14:00",
+    dayTime: 1594069200000,
+  },
+];
 
 const Day = ({ id }) => {
   const idArr = new Array(24).fill(0).map((el, index) => index);
 
   return (
     <div className="week-day">
-      {id === event.id ? <Event {...event} /> : null}
+      {events.map((event) =>
+        event.dayTime === id ? <Event key={event.id} {...event} /> : null
+      )}
       {idArr.map((id) => (
         <div className="hour" key={id} />
       ))}
