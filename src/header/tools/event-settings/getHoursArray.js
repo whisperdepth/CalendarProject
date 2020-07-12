355942682cbd231e239750d688a3ddd4e7b7c539
Roman Marchenko <whisperdepth@gmail.com>
2020-07-13 02:23:16 +0300

    d

--- src/header/tools/event-settings/getHoursArray.js
+++ src/header/tools/event-settings/getHoursArray.js
@@ -7,11 +7,9 @@ export default () => {
     return `${hours}:${minutes}`;
   };
 
-  let value = -15;
-  const minsArray = new Array(97).fill(15).map((hour) => {
-    value = hour + value;
-    return getTimeFromMins(value);
-  });
+  const minsArray = new Array(97)
+    .fill(15)
+    .map((hour, index) => getTimeFromMins(hour * index));
 
   return minsArray;
 };
