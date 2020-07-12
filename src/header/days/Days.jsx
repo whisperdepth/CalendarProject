f3a79c5d6d32984dd37e543fb2b5b20efe6f1123
Roman Marchenko <whisperdepth@gmail.com>
2020-07-12 21:05:20 +0300

    d

--- src/header/days/Days.jsx
+++ src/header/days/Days.jsx
@@ -4,26 +4,22 @@ import GMT from "./GMT/GMT";
 import Day from "./day/Day";
 import moment from "moment";
 
-class Days extends React.Component {
-  render() {
-    const { monday, week } = this.props;
-
-    return (
-      <div className="days">
-        <GMT monday={monday} />
-        <ul className="days-list">
-          {week.map((day) => (
-            <Day
-              key={day.getTime()}
-              name={moment(day).format("ddd")}
-              date={moment(day).format("DD")}
-              id={day.getTime()}
-            />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const Days = ({ monday, week }) => {
+  return (
+    <div className="days">
+      <GMT monday={monday} />
+      <ul className="days-list">
+        {week.map((day) => (
+          <Day
+            key={day.getTime()}
+            name={moment(day).format("ddd")}
+            date={moment(day).format("DD")}
+            id={day.getTime()}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Days;
--- src/header/tools/event-settings/save-btn/SaveBtn.jsx
+++ src/header/tools/event-settings/save-btn/SaveBtn.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import "./save-btn.scss";
 
-class SaveBtn extends React.Component {
-  
-  render() {
-    const { readInputsData, isButtonAvailable } = this.props;
-    return (
-      <button
-        disabled={isButtonAvailable}
-        onClick={readInputsData}
-        className="save-btn"
-      >
-        Save
-      </button>
-    );
-  }
-}
+const SaveBtn = ({ readInputsData, isButtonAvailable }) => {
+  return (
+    <button
+      disabled={isButtonAvailable}
+      onClick={readInputsData}
+      className="save-btn"
+    >
+      Save
+    </button>
+  );
+};
 
 export default SaveBtn;
