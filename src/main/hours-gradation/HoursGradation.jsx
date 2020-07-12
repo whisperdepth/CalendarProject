43deef359812c013472203d8674f7bcb92c3b086
Roman Marchenko <whisperdepth@gmail.com>
2020-07-12 21:10:00 +0300

    e

--- src/main/hours-gradation/HoursGradation.jsx
+++ src/main/hours-gradation/HoursGradation.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./hours-gradation.scss";
 
+const hoursArray = new Array(24).fill(0).map((el, index) => {
+  if (index === 0) return "";
+  return index.toString().split("").length > 1 ? `${index}:00` : `0${index}:00`;
+});
+
 const HoursGradation = () => {
-  const hoursArray = new Array(24).fill(0).map((el, index) => {
-    if (index === 0) return "";
-    return index.toString().split("").length > 1
-      ? `${index}:00`
-      : `0${index}:00`;
-  });
-  
   return (
     <div className="hours-gradation">
       {hoursArray.map((hour) => (
