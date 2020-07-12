62faeb0da66d3e77b4219b9659276fb359fc6977
Roman Marchenko <whisperdepth@gmail.com>
2020-07-12 20:58:28 +0300

    d

--- src/main/day/Day.jsx
+++ src/main/day/Day.jsx
@@ -3,37 +3,34 @@ import "./day.scss";
 import Event from "./event/Event";
 import Arrow from "./arrow/Arrow";
 
-class Day extends React.Component {
-  render() {
-    const { id, events, handleEventDelete } = this.props;
-    const idArr = new Array(24).fill(0).map((el, index) => index);
+const idArr = new Array(24).fill(0).map((el, index) => index);
 
-    return (
-      <div className="week-day">
-        {new Date(
-          new Date().getFullYear(),
-          new Date().getMonth(),
-          new Date().getDate()
-        ).getTime() === id ? (
-          <Arrow />
-        ) : null}
+const Day = ({ id, events, handleEventDelete }) => {
+  return (
+    <div className="week-day">
+      {new Date(
+        new Date().getFullYear(),
+        new Date().getMonth(),
+        new Date().getDate()
+      ).getTime() === id ? (
+        <Arrow />
+      ) : null}
 
-        {events.map((event) => {
-          return event.dayTime === id ? (
-            <Event
-              handleEventDelete={handleEventDelete}
-              key={event.id}
-              {...event}
-            />
-          ) : null;
-        })}
+      {events.map((event) => {
+        return event.dayTime === id ? (
+          <Event
+            handleEventDelete={handleEventDelete}
+            key={event.id}
+            {...event}
+          />
+        ) : null;
+      })}
 
-        {idArr.map((id) => (
-          <div className="hour" key={id} />
-        ))}
-      </div>
-    );
-  }
-}
+      {idArr.map((id) => (
+        <div className="hour" key={id} />
+      ))}
+    </div>
+  );
+};
 
 export default Day;
